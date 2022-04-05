6930703a957d2f57a881c6fc5dc3c71a8cdbec32
Odile <odille1998@gmail>
2022-04-05 17:11:10 +0200

    polling app in django

---
+++ polls/views.py
@@ -0,0 +1,43 @@
+from urllib import response
+from django import http
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question,Choice
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST[Choice])
+    except(KeyError, Choice.DoesNotExist):
+     # Redisplay the question voting form.
+        return render(request, 'polls/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
